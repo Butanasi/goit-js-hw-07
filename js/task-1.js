4cb8d3a42516eedf9c0f13edb7a3596136b2f98c
Danylo Nehreba <butanasiland@gmail.com>
2021-09-19 18:01:14 +0200

    task - 8

--- js/task-1.js
+++ js/task-1.js
@@ -1,21 +1,5 @@
-const ulEl = document.querySelector('#categories');
-console.log(ulEl);
 
-
-const liRef = document.querySelectorAll('.item');
-console.log(liRef);
-console.log('Кількість елементів:', liRef.length);
-
-
-const titleRef = document.querySelectorAll('.item h2');
-console.log(titleRef);
-console.log(titleRef[0].textContent);
-console.log(titleRef[1].textContent);
-console.log(titleRef[2].textContent);
-
-console.log(liRef[0].children);
-console.log(liRef[0].childNodes.length);
-console.log(liRef[1].children);
-console.log(liRef[1].childNodes.length);
-console.log(liRef[2].children);
-console.log(liRef[2].childNodes.length);
+const itemEl = document.querySelectorAll('.item h2')
+itemEl.forEach(item =>
+        console.log(`Категория: ${item.textContent}, 
+        Количество элементов: ${item.nextElementSibling.children.length}`))
